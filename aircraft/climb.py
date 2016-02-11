c57d13f8e34aecdd667a25688399e2dc72708002
pgkirsch <kirschen@mit.edu>
2016-02-11 01:04:22 -0500

    first working discretized climb

--- aircraft/climb.py
+++ aircraft/climb.py
@@ -1,4 +1,4 @@
-from gpkit import Variable, Model
+from gpkit import Variable, VectorVariable, Model
 import numpy as np
 
 class Climb(Model):
@@ -26,7 +26,7 @@ class Climb(Model):
         h = np.linspace(0, H, n)
         T0 = 288.15 # [K]
         L = 0.0065 # [K/m]
-        p0 = 101325 # [Pa]
+        p0 = 101325 # [Pa]
         R = 8.31 # [J/(mol*K)]
         g = 9.81 # [m/s^2]
         M = 0.02896 # [kg/mol]
@@ -36,16 +36,14 @@ class Climb(Model):
         rho = p*M/(R*T) # [kg/m^3]
 
         # Free variables
-        dt  = Variable('\\Delta t', 's', 'Time to climb segment')
-#        t   = Variable('t', 's', 'Time to climb')
-        D   = Variable('D', 'N', 'Drag')
-        m_f = VectorVariable('m_f', n, 'kg', 'Mass of fuel burned')
+        dt  = VectorVariable(n, '\\Delta t', 's', 'Time to climb segment')
+        D   = VectorVariable(n, 'D', 'N', 'Drag')
+        m_f = VectorVariable(n, 'm_f', 'kg', 'Mass of fuel burned')
 
         # Fixed parameters
         CD   = Variable('C_D', 0.02, '-', 'Drag coefficient')
-        dh   = Variable('\\Delta h', h/n, 'ft', 'Altitude step')
-#        h    = VectorVariable('h', h, 'm', 'Final altitude')
-        rho  = VectorVariable('\\rho', rho, 'kg/m^3', 'Air density')
+        dh   = Variable('\\Delta h', H/n, 'ft', 'Altitude step')
+        rho  = VectorVariable(n, '\\rho', rho, 'kg/m^3', 'Air density')
         S    = Variable('S', 130, 'm^2', 'Reference area')
         T    = Variable('T', 1E6, 'N', 'Thrust')
         TSFC = Variable('TSFC', 8.7E-4, 'g/(kN*s)', # [3]
@@ -53,7 +51,7 @@ class Climb(Model):
         V    = Variable('V', 100, 'm/s', 'Freestream velocity')
         W    = Variable('W', 600000, 'N', 'Aircraft weight')
 
-        objective = m_f
+        objective = sum(m_f)
 
         constraints = [# Climb time [1]
                        # uses Maclaurin series expansion of 1/(1-T/D) (ref [2])
