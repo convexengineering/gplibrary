1c6853a31cee6fa26f8c4755a412fc545bb3747d
Martin York <mayork@mit.edu>
2016-06-18 10:13:00 -0400

    Revert "trying ot link with atm"
    This reverts commit f42a93eee7cd03aadf0aa5cc00073c147c049bc3.

--- gpkitmodels/Commercial Sizing/Commerical_Flight_Profile.py
+++ gpkitmodels/Commercial Sizing/Commerical_Flight_Profile.py
@@ -9,39 +9,41 @@ import matplotlib.pyplot as plt
 from atmosphere import Troposphere, Tropopause
 
 #TODO
-#integrate atmosphere model
-#integrate engine model
 
-GAS_CONSTANT = 287  # [J/(kg*K)]
-GRAVITATIONAL_ACCEL = 9.81 # [m/s^2]
+class CommericalAircraft(Model):
+    """
+    minimizes the aircraft total weight, must specify all weights except fuel weight, so in effect
+    we are minimizing the fuel weight
 
-#define the number of discretizations for each flight segment
+    Rate of climb equation taken from John Anderson's Aircraft Performance and Design (eqn 5.85)
+    """
+    def __init__(self):
+        #define the number of discretizations for each flight segment
+        Ntakeoff = 0
+        Nclimb1 = 3
+        Ntrans = 0
+        Nclimb2 = 3
+        Ncruise1 = 0
+        Ncruiseclimb = 0
+        Ncruise2 = 3
+        Ndecent = 0
+        Nlanding = 0
+        #segments below here that start with res --> fuel reserves
+        Nresclimb = 0
+        Nresdecent = 0
+        Nreslanding = 0
+        Nreshold = 0
 
-Ntakeoff = 0
-Nclimb1 = 3
-Ntrans = 0
-Nclimb2 = 3
-Ncruise1 = 0
-Ncruiseclimb = 0
-Ncruise2 = 3
-Ndecent = 0
-Nlanding = 0
-#segments below here that start with res --> fuel reserves
-Nresclimb = 0
-Nresdecent = 0
-Nreslanding = 0
-Nreshold = 0
+        #total number of flight segments in each category, possibly useful
+        Nclimb = Nclimb1 + Nclimb2 + Ncruiseclimb
+        Ncruise = Ncruise1 + Ncruise2
+        Nres = Nresclimb + Nresdecent + Nreslanding + Nreshold
 
-#total number of flight segments in each category, possibly useful
-Nclimb = Nclimb1 + Nclimb2 + Ncruiseclimb
-Ncruise = Ncruise1 + Ncruise2
-Nres = Nresclimb + Nresdecent + Nreslanding + Nreshold
+        Nseg = Nclimb + Ncruise + Nres + Ntakeoff + Ntrans + Ndecent + Nlanding
 
-Nseg = Nclimb + Ncruise + Nres + Ntakeoff + Ntrans + Ndecent + Nlanding
+        #create index lists for the vector variables
 
-#create index lists for the vector variables
-
-#full flight profile
+        #full flight profile
 ##        itakeoff = map(int, np.linspace(0, Ntakeoff - 1, Ntakeoff))
 ##        iclimb1 = map(int, np.linspace(Ntakeoff, itakeoff[len(itakeoff)-1]+Nclimb1, Nclimb1))
 ##        itrans = map(int, np.linspace(iclimb1[len(iclimb1)-1] + 1, Ntrans + iclimb1[len(iclimb1)-1], Ntrans))
@@ -56,28 +58,20 @@ Nseg = Nclimb + Ncruise + Nres + Ntakeoff + Ntrans + Ndecent + Nlanding
 ##        ireslanding = map(int, np.linspace(iresdecent[len(iresdecent)-1] + 1, Nreslanding + iresdecent[len(iresdecent)-1], Nreslanding))
 ##        ireshold = map(int, np.linspace(ireslanding[len(ireslanding)-1] + 1, Nreshold + ireslanding[len(ireslanding)-1], Nresdecent))
 
-#partial flight profile for use during development
-iclimb1 = map(int, np.linspace(0, Nclimb1 - 1, Nclimb1))
-iclimb2 = map(int, np.linspace(iclimb1[len(iclimb1)-1] + 1, Nclimb2 + iclimb1[len(iclimb1)-1], Nclimb2))
-icruise2 = map(int, np.linspace(iclimb2[len(iclimb2)-1] + 1, Ncruise2 + iclimb2[len(iclimb2)-1], Ncruise2))
+        #partial flight profile for use during development
+        iclimb1 = map(int, np.linspace(0, Nclimb1 - 1, Nclimb1))
+        iclimb2 = map(int, np.linspace(iclimb1[len(iclimb1)-1] + 1, Nclimb2 + iclimb1[len(iclimb1)-1], Nclimb2))
+        icruise2 = map(int, np.linspace(iclimb2[len(iclimb2)-1] + 1, Ncruise2 + iclimb2[len(iclimb2)-1], Ncruise2))
 
-izbre = map(int, np.linspace(0, Ncruise2 - 1, Ncruise2))
-
-class FlightProfile(Model):
-    """
-    minimizes the aircraft total weight, must specify all weights except fuel weight, so in effect
-    we are minimizing the fuel weight
-
-    Rate of climb equation taken from John Anderson's Aircraft Performance and Design (eqn 5.85)
-    """
-    def __init__(self, **kwargs):
+        izbre = map(int, np.linspace(0, Ncruise2 - 1, Ncruise2))
+        
         #---------------------------------------
         #Variable definitions
         
         #physical constants
-        g   = Variable('g', GRAVITATIONAL_ACCEL, 'm/s^2', 'Gravitational acceleration')
+        g = Variable('g', 9.81, 'm/s^2', 'Gravitational Acceleration')
         gamma = Variable('\gamma', 1.4, '-', 'Air Specific Heat Ratio')
-        R   = Variable('R', GAS_CONSTANT, 'J/(kg*K)', 'Specific gas constant (air)')
+        R = Variable('R', 287, 'J/kg/K', 'Gas Constant for Air')
 
         #altitude
         hft = VectorVariable(Nseg, 'hft', 'feet', 'Altitude [feet]')
@@ -86,10 +80,9 @@ class FlightProfile(Model):
 
         #air properties
         a = VectorVariable(Nseg, 'a', 'm/s', 'Speed of Sound')
-        mu  = VectorVariable(Nseg, '\\mu', 'kg/(m*s)', 'Dynamic viscosity')
-        p   = VectorVariable(Nseg, 'p', 'Pa', 'Pressure')
-        rho = VectorVariable(Nseg, '\\rho', 'kg/m^3', 'Density')
-        T   = VectorVariable(Nseg, 'T', 'K', 'Temperature')
+        rho = VectorVariable(Nseg, '\rho', 'kg/m^3', 'Air Density')
+        mu = VectorVariable(Nseg, '\mu', 'kg/m/s', 'Air Kinematic Viscosity')
+        T = VectorVariable(Nseg, 'T', 'K', 'Air Temperature')
 
         #time
         tmin = VectorVariable(Nseg, 'tmin', 'min', 'Flight Time in Minutes')
@@ -99,7 +92,7 @@ class FlightProfile(Model):
         RngClimb = VectorVariable(Nclimb, 'RngClimb', 'miles', 'Segment Range During Climb')
         RngCruise = VectorVariable(Ncruise2 + Ncruise1, 'RngCCruise', 'miles', 'Segment Range During Cruise')
         ReqRngCruise = Variable('ReqRngCruise', 'miles', 'Required Cruise Range')
-        ReqRng = Variable('ReqRng', 'miles', 'Required Mission Range')
+        ReqRng = Variable('ReqRng', 300, 'miles', 'Required Mission Range')
 
         #aircraft weights
         W_e = Variable('W_{e}', 'lbf', 'Empty Weight of Aircraft')
@@ -112,23 +105,23 @@ class FlightProfile(Model):
 
         #aero
         LD = VectorVariable(Nseg, '\\frac{L}{D}', '-', 'Lift to Drag')
-        LDmax = Variable('\\frac{L}{D}_{max}', '-', 'Maximum Lift to Drag')
+        LDmax = Variable('\\frac{L}{D}_{max}', 15, '-', 'Maximum Lift to Drag')
 
         #CHECK VALUE OF CD0, K
         
-        Cd0 = Variable('C_{d_0}', '-', 'Aircraft Cd0')
-        K = Variable('K', '-', 'K for Parametric Drag Model')
+        Cd0 = Variable('C_{d_0}', .025, '-', 'Aircraft Cd0')
+        K = Variable('K', .9, '-', 'K for Parametric Drag Model')
 
         #aircraft geometry
 
         #CHECK THE VALUE OF S
         
-        S = Variable('S', 'm^2', 'Wing Planform Area')
+        S = Variable('S', 124.58, 'm^2', 'Wing Planform Area')
 
         #velocitites and mach numbers
         V = VectorVariable(Nseg, 'V', 'knots', 'Aircraft Flight Speed')
         M = VectorVariable(Nseg, 'M', '-', 'Aircraft Mach Number')
-        Vstall = Variable('V_{stall}', 'knots', 'Aircraft Stall Speed')
+        Vstall = Variable('V_{stall}', 120, 'knots', 'Aircraft Stall Speed')
 
         #Climb/Decent Rate
         RC = VectorVariable(Nseg, 'RC', 'feet/min', 'Rate of Climb/Decent')
@@ -141,7 +134,7 @@ class FlightProfile(Model):
         TSFC = VectorVariable(Nseg, 'TSFC', 'lb/hr/lbf', 'Thrust Specific Fuel Consumption')
 
         #currently sets the value of TSFC, just a place holder
-        c1 = Variable('c1', 'lb/lbf/hr', 'Constant')
+        c1 = Variable('c1', 2, 'lb/lbf/hr', 'Constant')
 
         with gpkit.SignomialsEnabled():
         
@@ -155,13 +148,13 @@ class FlightProfile(Model):
             constraints.extend([
                 #speed of sound
                 a  == (gamma * R * T)**.5,
-                T == 273*units('K'),
+                T ==273*units('K'),
                 
                 #convert m to ft
-                hft == h,
+                hft  == h,
                 
                 #convert min to hours
-                tmin == thours,
+                tmin  == thours ,
                 
                 #constraints on the various weights
                 W_e + W_payload + W_ftotal <= W_total,
@@ -177,7 +170,13 @@ class FlightProfile(Model):
                 ReqRngCruise   <= sum(RngCruise),
 
                 #altitude matching constraints
-                hft[icruise2]==hft[Nclimb-1]
+                hft[icruise2]==hft[Nclimb-1],
+
+                #substitute these values later
+                W_e  == 90000*units('lbf'),
+                W_payload  == 50000*units('lbf'),
+
+                W_ftotal>=W_e
                 ])
 
             #constrain the segment weights in a loop
@@ -211,13 +210,17 @@ class FlightProfile(Model):
                 theta[iclimb1]*V[iclimb1]  == RC[iclimb1],
                
                 dhft[iclimb1]  == tmin[iclimb1] * RC[iclimb1],
-                
                 #compute the distance traveled for each segment
+
                 #takes into account two terms of a cosine expansion
                 RngClimb[iclimb1] + .5*thours[iclimb1]*V[iclimb1]*theta[iclimb1]**2 <= thours[iclimb1]*V[iclimb1],
                 
                 #compute fuel burn from TSFC
-                W_fuel[iclimb1]  == g * TSFC[iclimb1] * thours[iclimb1] * thrust
+                W_fuel[iclimb1]  == g * TSFC[iclimb1] * thours[iclimb1] * thrust,
+
+                #sub these later
+                TSFC[iclimb1]  == c1,
+                rho[iclimb1] == 1.225*units('kg/m^3')
                 ])
             
             for i in range(0, Nclimb):
@@ -256,7 +259,11 @@ class FlightProfile(Model):
                 RngClimb[iclimb2] + .5*thours[iclimb2]*V[iclimb2]*theta[iclimb2]**2 <= thours[iclimb2]*V[iclimb2],
 
                 #compute fuel burn from TSFC
-                W_fuel[iclimb2]  == g * TSFC[iclimb2] * thours[iclimb2] * thrust
+                W_fuel[iclimb2]  == g * TSFC[iclimb2] * thours[iclimb2] * thrust,
+
+                #sub these later
+                TSFC[iclimb2]  == c1,
+                rho[iclimb2] == 1.225*units('kg/m^3')
                 ])
                      
             #--------------------------------------
@@ -273,7 +280,7 @@ class FlightProfile(Model):
 
             constraints.extend([
                 #constrain the climb rate by holding altitude constant
-                hft[icruise2]  == 11000*units('m'),
+                hft[icruise2]  == 35000*units('ft'),
                 
                 #taylor series expansion to get the weight term
                 W_fuel[icruise2]/W_end[icruise2] >= te_exp_minus1(z_bre[izbre], nterm=3),
@@ -282,7 +289,13 @@ class FlightProfile(Model):
                 RngCruise[izbre] <= z_bre[izbre]*LD[icruise2]*V[icruise2]/(TSFC[icruise2]*g),
                 
                 #time
-                thours[icruise2]*V[icruise2]  == RngCruise[izbre]
+                thours[icruise2]*V[icruise2]  == RngCruise[izbre],
+
+
+                #substitue these values later
+                TSFC[icruise2]  == 1.4*units('lb/lbf/hr'),
+                LD[icruise2]  == 10,
+                V[icruise2]  == 420*units('kts')
                 ])
             
             #constraint on the aircraft meeting the required range
@@ -290,20 +303,6 @@ class FlightProfile(Model):
                 constraints.extend([
                      RngCruise[i]   >= (i+1) * ReqRngCruise/Nseg
                     ])
-
-
-#crude replacement for substitions
-            constraints.extend([
-                #substitue these values later
-                TSFC[icruise2]  == 1.4*units('lb/lbf/hr'),
-                TSFC[iclimb1] == c1,
-                LD[icruise2]  == 10,
-                V[icruise2]  == 420*units('kts'),
-                TSFC[iclimb2]  == c1,
-                W_e  == 90000*units('lbf'),
-                W_payload  == 50000*units('lbf'),
-            #    rho == 1.225*units('kg/m^3')
-                ])
                    
             #---------------------------------------
             #decent
@@ -315,42 +314,9 @@ class FlightProfile(Model):
         #objective is to minimize the total required fuel weight, accomplished my minimizing the total weight
         objective =  W_total
 
-        Model.__init__(self, objective, constraints, **kwargs)
+        m = Model(objective, constraints)
 
-class CommercialFlight(Model):
-    """
-    class to link all the models necessary to simualte an actual commercial flight
-    """
-
-    def __init__(self, **kwargs):
-        #create all the submodels
-        
-        fltprofile = FlightProfile()
-        atm = Troposphere()
-
-        self.submodels = [fltprofile, atm]
-
-        substitutions = {
-            'S': 124.58,
-            '\\frac{L}{D}_{max}': 15,
-            'K': 0.9,
-            'C_{d_0}': .025,
-            'ReqRng': 1500,
-            'V_{stall}': 120,
-            'c1': 30000,
-            }
-
-        lc = LinkedConstraintSet([self.submodels])
-
-        
-        Model.__init__(self, fltprofile.cost, lc, substitutions)
-        
-        
-
-if __name__ == "__main__":
-        comflt = CommercialFlight()
-
-        sol = comflt.localsolve(verbosity=4)
+        sol = m.localsolve(verbosity=4)
 
         plt.plot(np.cumsum(sol('tmin')), sol('hft'))
         plt.title('Altitude vs Time')
@@ -358,3 +324,5 @@ if __name__ == "__main__":
         plt.xlabel('Time [min]')
         plt.show()
 
+if __name__ == '__main__':
+    CommericalAircraft()
