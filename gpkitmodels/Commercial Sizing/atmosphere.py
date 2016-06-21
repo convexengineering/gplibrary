03e292ddd897e54f6d2da9bf753a811e23594490
Martin York <mayork@mit.edu>
2016-06-21 14:31:14 -0400

    fized problems iwth atmosphere

--- gpkitmodels/Commercial Sizing/atmosphere.py
+++ gpkitmodels/Commercial Sizing/atmosphere.py
@@ -1,18 +1,19 @@
 "Models for atmospheric quantities"
 import numpy as np
+from gpkit import units
 from gpkit import Variable, Model, units, SignomialsEnabled
-from gpkit.tools import te_exp_minus1
 from gpkit.constraints.tight import TightConstraintSet as TCS
 # pylint: disable=bad-whitespace
 
 GAS_CONSTANT = 287  # [J/(kg*K)]
 
-h   = Variable('h', 'm', 'Altitude')
+hu   = Variable('hu', '-', 'Altitude w/out units')
+h = Variable('h', 'm', 'Altitude [meters]')
 mu  = Variable('\\mu', 'kg/(m*s)', 'Dynamic viscosity')
 p   = Variable('p', 'Pa', 'Pressure')
-R   = Variable('R', GAS_CONSTANT, 'J/(kg*K)', 'Specific gas constant (air)')
-rho = Variable('\\rho', 'kg/m^3', 'Density')
 T   = Variable('T', 'K', 'Temperature')
+rho = Variable('\rho', 'kg/m^3', 'Air Density')
+R = Variable('R', GAS_CONSTANT, 'J/kg/K', 'Gas Constant for Air')
 
 
 class Atmosphere(Model):
@@ -39,19 +40,22 @@ class Atmosphere(Model):
             #constrain temperature with the ideal gas law
             T == p/(rho*R),
 
+            #bad way of handling units problem
+            hu*units('m') == h,
+            
             #pressure constraint
-            p**-1 >= (0.000162 * (h)**-6.78e-06+ 1.54e-10 * (h)**-2+ 2.87e-05 * (h)**0.00355+ 0.000812 *
-                      (h)**0.00022+ 0.000248 * (h)**8.7e-05+ 1.41e-15 * (h)**2.88 + 5.66e-08 * (h)**1.06)**(1/.58),
+            TCS([p**-.58*units('Pa^.58') >= (0.000162 * hu**-6.78e-06+ 1.54e-10 * hu**-2+ 2.87e-05 * (hu)**0.00355+ 0.000812 *
+                      (hu)**0.00022+ 0.000248 * (hu)**8.7e-05+ 1.41e-15 * (hu)**2.88 + 5.66e-08 * (hu)**1.06)]),
             
             #density constraint
-            rho**-1 <= (0.149 * (h)**-0.00179+ 4.74e-10 * (h)**2.47+ 0.132 * (h)**-0.00142 + 3.55e-28 *
-                       (h)**6.89+ 0.0882 * (h)**-0.00141+ 0.151 * (h)**-0.000969+ 0.126 * (h)**-0.00141+ 0.000232 *
-                       (h)**0.933)**(1/2.15),
+            TCS([rho**-2.15*units('(kg/m^3)^2.15') >= (0.149 * (hu)**-0.00179+ 4.74e-10 * (hu)**2.47+ 0.132 * (hu)**-0.00142 + 3.55e-28 *
+                       (hu)**6.89+ 0.0882 * (hu)**-0.00141+ 0.151 * (hu)**-0.000969+ 0.126 * (hu)**-0.00141+ 0.000232 *
+                       (hu)**0.933)]),
                         ]
         su = Sutherland()
         lc = su.link(constraints)
 
-        Model.__init__(self, objective, lc, **kwargs)
+        Model.__init__(self, None, lc, **kwargs)
 
 class Sutherland(Model):
     """
