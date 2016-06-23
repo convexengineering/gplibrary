7794c53a5a34d684309b8d0e3cf43f5f90c2e93e
Martin York <mayork@mit.edu>
2016-06-23 14:31:57 -0400

    LPC map done

--- gpkitmodels/aircraft/engine_components.py
+++ gpkitmodels/aircraft/engine_components.py
@@ -496,65 +496,62 @@ class LPCMap(Model):
     """
     def __init__(self, **kwargs):
         #Temperature Variables
-        Tt = Variable('T_t', 'K', 'Face Stagnation Temperature (Station 2 or 2.5)')
+        Tt2 = Variable('T_{t_2}', 'K', 'Stagnation Temperature at the Fan Inlet (2)')
         Tref = Variable('T_{ref}', 'K', 'Reference Stagnation Temperature')
 
         #Mass Flow Variables
-        mbar = Variable('m_{bar}', 'kg/s', 'Corrected Mass Flow')
-        mdot = Variable('m_{dot}', 'kg/s', 'Mass Flow')
-        mtild = Variable('m_{tild}', '-', 'Normalized Mass Flow')
-        mD = Variable('m_D', 'kg/s', 'On-Design Mass Flow')
+        mlc = Variable('m_{lc}', 'kg/s', 'LPC Corrected Mass Flow')
+        mCore = Variable('m_{core}', 'kg/s', 'Core Mass Flow')
+        mtildlc = Variable('m_{tild}', '-', 'LPC Normalized Mass Flow')
+        mCoreD = Variable('m_{core_D}', 'kg/s', 'On Design Core Mass Flow')
 
         #Pressure Variables
-        Pt = Variable('P_t', 'kPa', 'Face Stagnation Pressure (Station 2 or 2.5)')
+        Pt2 = Variable('P_{t_2}', 'kPa', 'Stagnation Pressure at the Fan Inlet (2)')
         Pref = Variable('P_{ref}', 'kPa', 'Reference Stagnation Pressure')
 
         #pressure ratio variables
-        ptild = Variable('p_{tild}', '-', 'Normalized Pressure Ratio')
-        pi = Variable('\pi', '-', 'Pressure Ratio')
-        piD = Variable('\pi_D', '-', 'On-Design Pressure Ratio')
+        ptildlc = Variable('p_{tild}', '-', 'LPC Normalized Pressure Ratio')
+        pilc = Variable('\pi_{lc}', '-', 'LPC Pressure Ratio')
+        pilcD = Variable('\pi_{lc_D}', '-', 'LPC On-Design Pressure Ratio')
         
         #Speed Variables
-        Nbar = Variable('N_{bar}', '-', 'Corrected Compressor/Fan Speed')
-        N = Variable('N', '-', 'Compressor/Fan Speed')
-        Ntild = Variable('N_{tild}', '-', 'Normalized Speed')
-        NbarD = Variable('N_{{bar}_D}', '-', 'On-Design Corrected Speed')
+        Nbarlc = Variable('N_{bar_lc}', '-', 'Corrected LPC Speed')
+        N1 = Variable('N_1', '-', 'LPC Speed')
+        Ntildlc = Variable('N_{tild}', '-', 'LPC Normalized Speed')
+        NbarDlc = Variable('N_{{bar}_D_lc}', '-', 'LPC On-Design Corrected Speed')
 
         #Spine Paramterization Variables
-        mtilds = Variable('m_{{tild}_s}', '-', 'Spine Parameterization Variable')
-        ptilds = Variable('p_{{tild}_s}', '-', 'Spine Parameterization Variable')
+        mtildslc = Variable('m_{{tild}_slc}', '-', 'LPC Spine Parameterization Variable')
+        ptildslc = Variable('p_{{tild}_slc}', '-', 'LPC Spine Parameterization Variable')
 
         #te_exp_minus1 variable for B.287
-        z = Variable('z', '-', 'Taylor Expanded Variable to Replace Log Term')
+        zlc = Variable('zlc', '-', 'Taylor Expanded Variable to Replace Log Term in LPC Map')
 
-        #polytropic efficiecny
-        etaPol = Variable('\eta_{pol}', '-', 'Component Off Design Polytropic Efficiency')
-        etaPolD = Variable('\eta_{{pol}_D}', '-', 'Component On Design Polytropic Efficiency')
         with SignomialsEnabled():
             constraints = [
                 #define N bar
-                Nbar == N/((Tt/Tref)**.5), #B.279
+                Nbarlc == N1/((Tt2/Tref)**.5), #B.279
 
                 #define mbar
-                mbar == mdot*((Tt/Tref)**.5)/(Pt/Pref),    #B.280
+                mlc == mCore*((Tt2/Tref)**.5)/(Pt2/Pref),    #B.280
 
                 #define ptild
                 #SIGNOMIAL
-                SignomialEquality(ptild * (piD-1), (pi-1)),    #B.281
+                SignomialEquality(ptildlc * (pilcD-1), (pilc-1)),    #B.281
 
                 #define mtild
-                mtild == mbar/mD,   #B.282
+                mtildlc == mlc/mCoreD,   #B.282
 
                 #define N tild
-                Ntild == Nbar/NbarD,    #B.283
+                Ntildlc == Nbarlc/NbarDlc,    #B.283
 
                 #spine paramterization
-                mtilds == Nbar**.5, #B.284
-                ptilds == mtilds ** 1.5, #B.285
+                mtildslc == Nbarlc**.5, #B.284
+                ptildslc == mtildslc ** 1.5, #B.285
 
                 #constrain the "knee" shape of the map
-                SignomialEquality(((mtilds-mtild)/.03), te_exp_minus1(z, nterm=3)),  #B.286
-                SignomialEquality(ptild, 2*Ntild*.03*z + ptilds),    #B.286
+                SignomialEquality(((mtildslc-mtildlc)/.03), te_exp_minus1(zlc, nterm=3)),  #B.286
+                SignomialEquality(ptildlc, 2*Ntildlc*.03*zlc + ptildslc),    #B.286
                 ]
                 
             Model.__init__(self, 1/pi, constraints, **kwargs)
