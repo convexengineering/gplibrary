078a12e421e7cb625081957a0f9c6c817d626391
mjburton <mjburton@mit.edu>
2016-06-28 21:13:16 -0400

    changed M to model; substitute back old vars

--- 1682/solar_hale/plotting.py
+++ 1682/solar_hale/plotting.py
@@ -2,31 +2,35 @@ import numpy as np
 import matplotlib.pyplot as plt
 from gpkit.small_scripts import unitstr
 
-def poor_mans_contour(M, xvarname, xsweep, zvarname, zsweep, yvarname,
+def poor_mans_contour(model, xvarname, xsweep, zvarname, zsweep, yvarname,
                       ylimits):
 
     # save sold substitions substitute
-    x_old = M.substitutions[xvarname]
-    z_old = M.substitutions[zvarname]
+    x_old = model.substitutions[xvarname]
+    z_old = model.substitutions[zvarname]
 
     # substitute in new variables
-    M.substitutions.update({xvarname: ("sweep", xsweep)})
+    model.substitutions.update({xvarname: ("sweep", xsweep)})
 
     # create figure
     fig, ax = plt.subplots()
 
     # iterate through z variables
     for val in zsweep:
-        M.substitutions.update({zvarname: val})
-        sol = M.solve("mosek", skipsweepfailures=True)
+        model.substitutions.update({zvarname: val})
+        sol = model.solve("mosek", skipsweepfailures=True)
 
         ax.plot(sol(xvarname), sol(yvarname))
 
+    # return substitution values to what they were
+    model.substitutions.update({xvarname: x_old})
+    model.substitutions.update({zvarname: z_old})
+
     # format plot
-    ax.set_xlabel("%s [%s]" % (M[xvarname].descr["label"],
-                               unitstr(M[xvarname].units)))
-    ax.set_ylabel("%s [%s]" % (M[yvarname].descr["label"],
-                               unitstr(M[yvarname].units)))
+    ax.set_xlabel("%s [%s]" % (model[xvarname].descr["label"],
+                               unitstr(model[xvarname].units)))
+    ax.set_ylabel("%s [%s]" % (model[yvarname].descr["label"],
+                               unitstr(model[yvarname].units)))
     ax.set_ylim((ylimits[0], ylimits[1]))
 
     return fig, ax
