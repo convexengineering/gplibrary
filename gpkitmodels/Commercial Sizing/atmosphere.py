5f6ff536c61d9f75a6ef3a7317824a0df5591cc2
Martin York <mayork@mit.edu>
2016-07-01 12:41:52 -0400

    vectorized atmosphere model

--- gpkitmodels/Commercial Sizing/atmosphere.py
+++ gpkitmodels/Commercial Sizing/atmosphere.py
@@ -1,20 +1,11 @@
 "Models for atmospheric quantities"
 import numpy as np
 from gpkit import units
-from gpkit import Variable, Model, units, SignomialsEnabled
+from gpkit import VectorVariable, Variable, Model, units, SignomialsEnabled
 from gpkit.constraints.tight import TightConstraintSet as TCS
 # pylint: disable=bad-whitespace
 
 GAS_CONSTANT = 287  # [J/(kg*K)]
-
-hu   = Variable('hu', '-', 'Altitude w/out units')
-h = Variable('h', 'm', 'Altitude [meters]')
-mu  = Variable('\\mu', 'kg/(m*s)', 'Dynamic viscosity')
-p   = Variable('p', 'Pa', 'Pressure')
-T   = Variable('T', 'K', 'Temperature')
-rho = Variable('\rho', 'kg/m^3', 'Air Density')
-R = Variable('R', GAS_CONSTANT, 'J/kg/K', 'Gas Constant for Air')
-
 
 class Atmosphere(Model):
     """
@@ -33,7 +24,22 @@ class Atmosphere(Model):
     http://www.digitaldutch.com/atmoscalc/index.htm
 
     """
-    def __init__(self, **kwargs):
+    def __init__(self, N, **kwargs):
+        #indicies of the vector variables
+        index = map(int, np.linspace(1, N-1, N))
+        #define variables
+        hu   = VectorVariable(N, 'hu', '-', 'Altitude w/out units')
+        h = VectorVariable(N, 'h', 'm', 'Altitude [meters]')
+        mu  = VectorVariable(N, '\\mu', 'kg/(m*s)', 'Dynamic viscosity')
+        p   = VectorVariable(N, 'p', 'Pa', 'Pressure')
+        pu   = VectorVariable(N, 'pu', '-', 'Pressure w/out Units')
+        T   = VectorVariable(N, 'T', 'K', 'Temperature')
+        rho = VectorVariable(N, '\rho', 'kg/m^3', 'Air Density w/out Units')
+        rhou = VectorVariable(N, '\rhou', '-', 'Air Density')
+        R = Variable('R', GAS_CONSTANT, 'J/kg/K', 'Gas Constant for Air')
+        m = Variable('m', 1, 'm', '1 Meter')
+        kgm3 = Variable('kgm3', 1, 'kg/m^3', '1 kg/m^3')
+        pa =Variable('pa', 1, 'Pa', '1 Pa')
         constraints = []
 
         constraints += [
@@ -41,21 +47,24 @@ class Atmosphere(Model):
             T == p/(rho*R),
 
             #bad way of handling units problem
-            hu*units('m') == h,
+            hu*m == h,
+            pu*pa ==p,
+            rhou*kgm3 == rho,
             
             #pressure constraint
-            TCS([p**-.58*units('Pa^.58') >= (0.000162 * hu**-6.78e-06+ 1.54e-10 * hu**-2+ 2.87e-05 * (hu)**0.00355+ 0.000812 *
+            TCS([pu**-.58 >= (0.000162 * hu**-6.78e-06+ 1.54e-10 * hu**-2+ 2.87e-05 * (hu)**0.00355+ 0.000812 *
                       (hu)**0.00022+ 0.000248 * (hu)**8.7e-05+ 1.41e-15 * (hu)**2.88 + 5.66e-08 * (hu)**1.06)]),
             
             #density constraint
-            TCS([rho**-2.15*units('(kg/m^3)^2.15') >= (0.149 * (hu)**-0.00179+ 4.74e-10 * (hu)**2.47+ 0.132 * (hu)**-0.00142 + 3.55e-28 *
+            TCS([rhou**-2.15 >= (0.149 * (hu)**-0.00179+ 4.74e-10 * (hu)**2.47+ 0.132 * (hu)**-0.00142 + 3.55e-28 *
                        (hu)**6.89+ 0.0882 * (hu)**-0.00141+ 0.151 * (hu)**-0.000969+ 0.126 * (hu)**-0.00141+ 0.000232 *
                        (hu)**0.933)]),
-                        ]
-        su = Sutherland()
-        lc = su.link(constraints)
+            ]
+        
+##        su = Sutherland(N)
+##        lc = su.link(constraints)
 
-        Model.__init__(self, None, lc, **kwargs)
+        Model.__init__(self, None, constraints, **kwargs)
 
 class Sutherland(Model):
     """
@@ -66,13 +75,13 @@ class Sutherland(Model):
         atmos/atmos.html
     http://www.cfd-online.com/Wiki/Sutherland's_law
     """
-    def __init__(self, **kwargs):
-
-        T_s = Variable('T_s', 110.4, "K", "Sutherland Temperature")
-        C_1 = Variable('C_1', 1.458E-6, "kg/(m*s*K^0.5)",
+    def __init__(self, N, **kwargs):
+        index = map(int, np.linspace(1, N-1, N))
+        T_s = VectorVariable(N, 'T_s', 110.4, "K", "Sutherland Temperature")
+        C_1 = VectorVariable(N, 'C_1', 1.458E-6, "kg/(m*s*K^0.5)",
                        'Sutherland coefficient')
-
-        t_plus_ts_approx = (T + T_s).mono_approximation({T: 288.15,
+        T   = VectorVariable(N, 'T', 'K', 'Temperature')
+        t_plus_ts_approx[index] = (T[index] + T_s[index]).mono_approximation({T: 288.15,
                                                          T_s: T_s.value})
 
         objective = mu
@@ -82,7 +91,7 @@ class Sutherland(Model):
 
     @classmethod
     def test(cls):
-        m = cls()
+        m = cls(2)
         m.substitutions.update({"T": 288})
         m.solve()
 
