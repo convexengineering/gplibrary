7261f8b0c1991d199a9c82c75f53977a194f951c
mayork <mayork@mit.edu>
2016-08-08 20:37:07 -0400

    more realistic throttle settings

--- gpkitmodels/Commercial Sizing/engine_atm.py
+++ gpkitmodels/Commercial Sizing/engine_atm.py
@@ -47,7 +47,7 @@ class EngineOnDesign(Model):
             substitutions = {
 ##                'P_0': 30,  
                 'M_0': 0.8,
-                'T_{t_4}': 1400,
+                'T_{t_4}': 1300,
                 '\pi_f': 1.5,
                 '\pi_{lc}': 3,
                 '\pi_{hc}': 10,
@@ -118,7 +118,7 @@ class EngineOffDesign(Model):
 
             substitutions = {
                 'T_0_1': 240,#260,   #36K feet
-                'T_{t_{4spec}}': 1400,
+                'T_{t_{4spec}}': 1300,
 ##                'P_0_1': 88,  
 ##                'M_0': 0.8,
             }
@@ -157,7 +157,7 @@ class EngineOffDesign2(Model):
 
         substitutions = {
             'T_0_2': 240,#260,   #36K feet
-            'T_{t_{4spec}}': 1350,
+            'T_{t_{4spec}}': 1250,
 ##            'P_0_2': 76,  
 ##            'M_0_2': 0.8,
         }
@@ -196,7 +196,7 @@ class EngineOffDesign3(Model):
 
         substitutions = {
             'T_0_3': 240,#250,   #36K feet
-            'T_{t_{4spec}}': 1300,
+            'T_{t_{4spec}}': 1200,
 ##            'P_0_3': 57,  
 ##            'M_0_3': 0.8,
         }
