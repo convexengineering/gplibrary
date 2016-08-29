2c4738f1d09b213a60e013efe7fe8cca3341f9f9
mayork <mayork@mit.edu>
2016-08-29 09:49:58 -0400

    minor tweaks

--- gpkitmodels/Commercial Sizing/atmosphere.py
+++ gpkitmodels/Commercial Sizing/atmosphere.py
@@ -31,12 +31,12 @@ class Atmosphere(Model):
         index = map(int, np.linspace(1, N-1, N))
         #define variables
         hu   = VectorVariable(N, 'hu', '-', 'Altitude w/out units')
-        h = VectorVariable(N, 'h', 'm', 'Altitude [meters]')
+        h = VectorVariable(N, 'hClimb1', 'm', 'Altitude [meters]')
         mu  = VectorVariable(N, '\\mu', 'kg/(m*s)', 'Dynamic viscosity')
-        p   = VectorVariable(N, 'p', 'Pa', 'Pressure')
+        p   = VectorVariable(N, 'pClimb1', 'Pa', 'Pressure')
         pu   = VectorVariable(N, 'pu', '-', 'Pressure w/out Units')
-        T   = VectorVariable(N, 'T', 'K', 'Temperature')
-        rho = VectorVariable(N, '\rho', 'kg/m^3', 'Air Density w/out Units')
+        T   = VectorVariable(N, 'TClimb1', 'K', 'Temperature')
+        rho = VectorVariable(N, '\rhoClimb1', 'kg/m^3', 'Air Density w/out Units')
         rhou = VectorVariable(N, '\rhou', '-', 'Air Density')
         R = Variable('R', GAS_CONSTANT, 'J/kg/K', 'Gas Constant for Air')
         m = Variable('m', 1, 'm', '1 Meter')
@@ -50,42 +50,70 @@ class Atmosphere(Model):
 
         
         constraints = []
+    
+        constraints += [
+            #constrain temperature with the ideal gas law
+            T == p/(rho*R),
+
+            #bad way of handling units problem
+            hu*m == h,
+            pu*pa ==p,
+            rhou*kgm3 == rho,
+            ]
+        
         with SignomialsEnabled():
-            constraints += [
-                #constrain temperature with the ideal gas law
-##                T == p/(rho*R),
+                for i in range(N):
+                    constraints.extend([
+                        #pressure constraint
+##                        SignomialEquality(pu[i]**-.58, (0.000162 * hu[i]**-6.78e-06+ 1.54e-10 * hu[i]**-2+ 2.87e-05 * (hu[i])**0.00355+ 0.000812 *
+##                                  (hu[i])**0.00022+ 0.000248 * (hu[i])**8.7e-05+ 1.41e-15 * (hu[i])**2.88 + 5.66e-08 * (hu[i])**1.06)),
 ##
-##                #bad way of handling units problem
-##                hu*m == h,
-##                pu*pa ==p,
-##                rhou*kgm3 == rho,
-##                
-                #pressure constraint
-##                TCS([pu**-.58 <= (0.000162 * hu**-6.78e-06+ 1.54e-10 * hu**-2+ 2.87e-05 * (hu)**0.00355+ 0.000812 *
+##                        #density constraint
+##                        SignomialEquality(rhou[i]**(-2.15), (0.149 * (hu[i])**-0.00179+ 4.74e-10 * (hu[i])**2.47+ 0.132 * (hu[i])**-0.00142 + 3.55e-28 *
+##                                   (hu[i])**6.89+ 0.0882 * (hu[i])**-0.00141+ 0.151 * (hu[i])**-0.000969+ 0.126 * (hu[i])**-0.00141+ 0.000232 *
+##                                   (hu[i])**0.933)),
+
+                        rhou ==1,
+                        pu==100000,
+
+##                        pu[i]**-.58 >= (0.000162 * hu[i]**-6.78e-06+ 1.54e-10 * hu[i]**-2+ 2.87e-05 * (hu[i])**0.00355+ 0.000812 *
+##                                   (hu[i])**0.00022+ 0.000248 * (hu[i])**8.7e-05+ 1.41e-15 * (hu[i])**2.88 + 5.66e-08 * (hu[i])**1.06),
+##
+##                        #density constraint
+##                        rhou[i]**(-2.15) >= (0.149 * (hu[i])**-0.00179+ 4.74e-10 * (hu[i])**2.47+ 0.132 * (hu[i])**-0.00142 + 3.55e-28 *
+##                                   (hu[i])**6.89+ 0.0882 * (hu[i])**-0.00141+ 0.151 * (hu[i])**-0.000969+ 0.126 * (hu[i])**-0.00141+ 0.000232 *
+##                                   (hu[i])**0.933),
+                        ])
+
+
+##                p[index] == 0.195 * (h[index]*units('1/Pa'))**0.702,
+                    
+
+##                               TCS([pu**-.58 <= (0.000162 * hu**-6.78e-06+ 1.54e-10 * hu**-2+ 2.87e-05 * (hu)**0.00355+ 0.000812 *
 ##                          (hu)**0.00022+ 0.000248 * (hu)**8.7e-05+ 1.41e-15 * (hu)**2.88 + 5.66e-08 * (hu)**1.06)]),
 ##                p[index] == 0.195 * (h[index]*units('1/Pa'))**0.702,
-                #density constraint
+##                #density constraint
 ##                TCS([rhou**(-2.15) >= (0.149 * (hu)**-0.00179+ 4.74e-10 * (hu)**2.47+ 0.132 * (hu)**-0.00142 + 3.55e-28 *
 ##                           (hu)**6.89+ 0.0882 * (hu)**-0.00141+ 0.151 * (hu)**-0.000969+ 0.126 * (hu)**-0.00141+ 0.000232 *
 ##                           (hu)**0.933)]),
 
 
                 # Temperature lapse rate constraint
-                TCS([T_0 <= T + L*h]),
-                T >= 216.65*units('K'),
+##                TCS([T_0 <= T + L*h]),
+##                T >= 216.65*units('K'),
                 
                 # Pressure-altitude relation
 ##                (p/p_0)**(1/th) == T/T_0,
 
                 # Ideal gas law
 ##                rho == p/(R*T),
-                ]
+                
             #tomorrow try a one term fit for rho
-            array_sec = [SignomialEquality(rhou[i]**(-2.15), (0.149 * (hu[i])**-0.00179+ 4.74e-10 * (hu[i])**2.47+ 0.132 * (hu[i])**-0.00142 + 3.55e-28 *
-                           (hu[i])**6.89+ 0.0882 * (hu[i])**-0.00141+ 0.151 * (hu[i])**-0.000969+ 0.126 * (hu[i])**-0.00141+ 0.000232 *
-                           (hu[i])**0.933)) for i in range(len(hu))]
+##            array_sec = [SignomialEquality(rhou[i]**(-2.15), (0.149 * (hu[i])**-0.00179+ 4.74e-10 * (hu[i])**2.47+ 0.132 * (hu[i])**-0.00142 + 3.55e-28 *
+##                           (hu[i])**6.89+ 0.0882 * (hu[i])**-0.00141+ 0.151 * (hu[i])**-0.000969+ 0.126 * (hu[i])**-0.00141+ 0.000232 *
+##                           (hu[i])**0.933)) for i in range(len(hu))]
 
-            constraints.extend(array_sec)
+##            constraints.extend(array_sec)
         
 ##        su = Sutherland(N)
 ##        lc = su.link(constraints)
