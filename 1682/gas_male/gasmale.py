01d60169b98d7efbe1a91a498887ed0e9051277d
mjburton <mjburton@mit.edu>
2016-09-28 10:02:32 -0400

    compute force from max load case

--- 1682/gas_male/gasmale.py
+++ 1682/gas_male/gasmale.py
@@ -650,13 +650,14 @@ class EngineWeight(Model):
 class TailBoom(Model):
     def __init__(self, **kwargs):
 
-        F = Variable("F", 22.4, "N", "point force from tail")
+        F = Variable("F", 42.4, "N", "point force from tail")
         L = Variable("L", 4.67, "ft", "tail boom length")
         E = Variable("E", 150e9, "N/m^2", "young's modulus carbon fiber")
         kfac = Variable("(1-k/2)", 0.6, "-", "(1-k/2) tail boom inertia value")
         I0 = Variable("I_0", "m^4", "tail boom moment of inertia")
         d0 = Variable("d_0", "m", "tail boom diameter")
-        t0 = Variable("t_0", 0.25, "m", "tail boom thickness")
+        t0 = Variable("t_0", "mm", "tail boom thickness")
+        tmin = Variable("t_{min}", 0.25, "mm", "minimum tail boom thickness")
         rho_cfrp = Variable("\\rho_{CFRP}", 1.6, "g/cm^3", "density of CFRP")
         g = Variable("g", 9.81, "m/s^2", "Gravitational acceleration")
         W = Variable("W", "lbf", "tail boom weight")
@@ -666,6 +667,7 @@ class TailBoom(Model):
 
         constraints = [I0 == pi*t0*d0**3/8.0,
                        W >= pi*g*rho_cfrp*d0*L*t0*kfac,
+                       t0 >= tmin,
                        th <= thmax,
                        th >= F*L**2/E/I0*kfac
                       ]
@@ -673,14 +675,20 @@ class TailBoom(Model):
         Model.__init__(self, None, constraints, **kwargs)
 
 class Tail(Model):
-    def __init__(self, **kwargs):
+    def __init__(self, case, **kwargs):
         W_vtail = Variable("W_{v-tail}", 3.4999, "lbf", "V-Tail weight")
         m_fac = Variable("m_{fac}", 1.0, "-", "Tail weight margin factor")
         W = Variable("W", "lbf", "Tail weight")
+        Shtail = Variable("S_{htail}", 2.33, "ft**2", "horizontal tail area")
+        CLmax = Variable("C_{L-max}", 1.0, "-",
+                         "maximum lift of horizontal tail")
 
         tailboom = TailBoom()
 
-        constraints = [W/m_fac >= W_vtail + tailboom["W"]]
+        constraints = [
+            W/m_fac >= W_vtail + tailboom["W"],
+            tailboom["F"] >= 0.5*case["\\rho"][0]*case["V_{stall}"][0]**2*Shtail*CLmax
+            ]
 
         lc = LinkedConstraintSet([tailboom, constraints], exclude=["W"])
 
@@ -749,7 +757,7 @@ class GasMALE(Model):
         mission = Mission(h_station, wind, DF70, Nclimb, Nloiter)
         engineweight = EngineWeight(DF70)
         wing = Wing()
-        tail = Tail()
+        tail = Tail(mission.submodels[0])
         avionics = Avionics()
         fuselage = Fuselage()
         center_loads = [fuselage, avionics, engineweight]
