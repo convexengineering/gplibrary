3256c9f74de79874231768513156517183c99d2e
1ozturkbe <1ozturkbe@gmail.com>
2016-09-29 21:53:51 -0400

    Added systems thickness to walls, reformatted constraints (what a beauty!)

--- gpkitmodels/aircraft/d8fuselage.py
+++ gpkitmodels/aircraft/d8fuselage.py
@@ -45,7 +45,8 @@ class Fuselage(Model):
         wfloor  = Variable('w_{floor}', 'm', 'Floor width')
         wfuse   = Variable('w_{fuse}', 'm', 'Fuselage width')
         wseat   = Variable('w_{seat}',0.5,'m', 'Seat width')
-
+        wsys     = Variable('w_{sys}', 0.1,'m', 'Width between cabin and skin for systems')
+        
         # Tail cone variables
         bvt     = Variable('b_{vt}', 'm', 'Vertical tail span')
         lamcone = Variable('\\lambda_{cone}', '-','Tailcone radius taper ratio (xshell2->xtail)')
@@ -54,17 +55,17 @@ class Fuselage(Model):
         plamv   = Variable('p_{\\lambda_v}', '-', '1 + 2*Tail taper ratio')
         Qv      = Variable('Q_{v}', 'N*m', 'Torsion moment imparted by tail')
         tcone   = Variable('t_{cone}', 'm', 'Cone thickness')
-
+        
         # Lengths (free)
         lfuse  = Variable('l_{fuse}', 'm', 'Fuselage length')
         lnose  = Variable('l_{nose}', 'm', 'Nose length')
         lshell = Variable('l_{shell}', 'm', 'Shell length')
         lfloor = Variable('l_{floor}', 'm', 'Floor length')
-      
+        
         # Surface areas (free)
         Sbulk    = Variable('S_{bulk}', 'm^2', 'Bulkhead surface area')
         Snose    = Variable('S_{nose}', 'm^2', 'Nose surface area')
-
+        
         # Volumes (free)        
         Vbulk  = Variable('V_{bulk}', 'm^3', 'Bulkhead skin volume')
         Vcabin = Variable('V_{cabin}', 'm^3', 'Cabin volume')
@@ -73,7 +74,7 @@ class Fuselage(Model):
         Vdb    = Variable('V_{db}', 'm^3', 'Web volume')
         Vfloor = Variable('V_{floor}', 'm^3', 'Floor volume')
         Vnose  = Variable('V_{nose}', 'm^3', 'Nose skin volume')
-
+        
         # Weights
         #Wbuoy    = Variable('W_{buoy}', 'N', 'Buoyancy weight')
         Wavgpass = Variable('W_{avg. pass}', 180, 'lbf', 'Average passenger weight')
@@ -90,17 +91,17 @@ class Fuselage(Model):
         Wshell   = Variable('W_{shell}','N','Shell weight')
         Wskin    = Variable('W_{skin}', 'N', 'Skin weight')
         Wwindow  = Variable('W_{window}', 'N', 'Window weight')
-
+        
         # Weight fractions (fixed, with respect to the aircraft skin weight, set from PRSEUS metrics)
-
+        
         #ffadd     = Variable('f_{fadd}', '-','Fractional added weight of local reinforcements')
         ffairing  = Variable('f_{fairing}',0.151,'-','  Fractional fairing weight')
         fframe    = Variable('f_{frame}',0.634,'-', 'Fractional frame weight')
         fstring   = Variable('f_{string}',0.235,'-','Fractional stringer weight')
         fwebcargo = Variable('f_{web}',1.030, '-','Fractional web and cargo floor weight')
-
+        
         # Misc free variables
-
+        
         # Material properties
         rE       = Variable('r_E', 1,'-', 'Ratio of stringer/skin moduli')
         rhobend  = Variable('\\rho_{bend}',2700, 'kg/m^3', 'Stringer density')
@@ -112,7 +113,7 @@ class Fuselage(Model):
         Wppinsul = Variable('W\'\'_{insul}',22,'N/m^2', 'Weight/area density of insulation material')
         Wpseat   = Variable('W\'_{seat}',150,'N', 'Weight per seat')
         Wpwindow = Variable('W\'_{window}', 145.*3,'N/m', 'Weight/length density of windows')
-
+        
         # Loads
         #Pcargofloor = Variable ('P_{cargo floor}','N','Distributed cargo floor load')
         Mfloor   = Variable('M_{floor}', 'N*m', 'Max bending moment in floor beams')
@@ -121,7 +122,7 @@ class Fuselage(Model):
         sigfloor = Variable('\\sigma_{floor}',30000/0.000145, 'N/m^2', 'Max allowable floor stress') #TASOPT value used
         sigskin  = Variable('\\sigma_{skin}', 46000,'psi', 'Max allowable skin stress') # again notional 
         sigth    = Variable('\\sigma_{\\theta}', 'N/m^2', 'Skin hoop stress')
-        sigx = Variable('\\sigma_x', 'N/m^2', 'Axial stress in skin')
+        sigx     = Variable('\\sigma_x', 'N/m^2', 'Axial stress in skin')
         taucone  = Variable('\\tau_{cone}', 'N/m^2', 'Shear stress in cone')
         taufloor = Variable('\\tau_{floor}',30000/0.000145, 'N/m^2', 'Max allowable shear web stress') #TASOPT value used
         
@@ -134,20 +135,20 @@ class Fuselage(Model):
             constraints = [
             # Passenger constraints (assuming 737-sixed aircraft)
             #Temporarily
-            Wpay == 150000*units('N'),
-            Wseat == 50000*units('N'),
-            nrows == nseats/SPR,
+            Wpay   == 150000*units('N'),
+            Wseat  == 50000*units('N'),
+            nrows  == nseats/SPR,
             lshell == nrows*pitch,
 
             # Fuselage joint angle relations
             thetadb == wdb/Rfuse, # first order Taylor works...
             thetadb >= 0.05, thetadb <= 0.25,
-            hdb >= Rfuse*(1.0-.5*thetadb**2), #[SP]
+            hdb     >= Rfuse*(1.0-.5*thetadb**2), #[SP]
 
             # Fuselage cross-sectional relations
-            Askin >= (2*pi + 4*thetadb)*Rfuse*tskin + Adb, #no delta R for now
-            Adb == (2*hdb)*tdb,
-            Afuse >= (pi + 2*thetadb + thetadb)*Rfuse**2,
+            Askin  >= (2*pi + 4*thetadb)*Rfuse*tskin + Adb, #no delta R for now
+            Adb    == (2*hdb)*tdb,
+            Afuse  >= (pi + 2*thetadb + thetadb)*Rfuse**2,
             tshell >= tskin*(1+rE*fstring*rhoskin/rhobend),
             
             # Fuselage surface area relations
@@ -155,35 +156,34 @@ class Fuselage(Model):
             Sbulk >= (2*pi + 4*thetadb)*Rfuse**2,
 
             # Fuselage length relations
-            lfuse >= lnose+lshell+lcone, # forget about tailcone for now
-            # Temporarily
-            lnose == 0.3*lshell,
+            lfuse >= lnose+lshell+lcone, 
+            lnose == 0.3*lshell, # Temp. 
             lcone == Rfuse/lamcone,
 
             # Fuselage width relations
-            wfuse >= SPR*wseat + 2*waisle + tdb + 2*tskin,
-            wfuse <= 2*(Rfuse + wdb),
+            wfuse  >= SPR*wseat + 2*waisle + tdb + 2*tshell + 2*wsys,
+            wfuse  <= 2*(Rfuse + wdb),
             wfloor >= wdb + Rfuse, # half of the total floor width in fuselage
 
             # Fuselage volume relations
-            Vcyl == Askin*lshell,
-            Vnose == Snose*tskin,
-            Vbulk == Sbulk*tskin,
-            Vdb == Adb*lshell,
+            Vcyl   == Askin*lshell,
+            Vnose  == Snose*tskin,
+            Vbulk  == Sbulk*tskin,
+            Vdb    == Adb*lshell,
             Vcabin >= Afuse*(lshell + 0.67*lnose + 0.67*Rfuse),
 
             # Fuselage weight relations
-            Winsul >= Wppinsul*((1.1*pi+2*thetadb)*Rfuse*lshell + 0.55*(Snose+Sbulk)),
+            Winsul  >= Wppinsul*((1.1*pi+2*thetadb)*Rfuse*lshell + 0.55*(Snose+Sbulk)),
             Wwindow >= Wpwindow*lshell,
-            Wskin >= rhoskin*g*(Vcyl + Vnose + Vbulk),
-            Wshell >= Wskin*(1 + fstring + ffairing + fframe +fwebcargo),
-            Wfuse >= Wfix + Winsul + Wshell + Wfloor + Wcone + Wwindow,
+            Wskin   >= rhoskin*g*(Vcyl + Vnose + Vbulk),
+            Wshell  >= Wskin*(1 + fstring + ffairing + fframe +fwebcargo),
+            Wfuse   >= Wfix + Winsul + Wshell + Wfloor + Wcone + Wwindow,
             
             ## Stress relations
             #Pressure shell loading
             tskin == dPover*Rfuse/sigskin,
-            tdb == 2*dPover*wdb/sigskin,
-            sigx == dPover*Rfuse/(2*tshell),
+            tdb   == 2*dPover*wdb/sigskin,
+            sigx  == dPover*Rfuse/(2*tshell),
             sigth == dPover*Rfuse/tskin,
 
             # Floor loading (don't understand some of these relations,
@@ -203,14 +203,14 @@ class Fuselage(Model):
             Ivshell <= (pi*Rfuse**2 + 8*wdb*Rfuse + (2*pi+4*thetadb)*wdb**2)*Rfuse*tshell, #approx needs to be improved [SP]
             
             # Tail cone loading model
-            Lvmax == 35000*units('N'), # based on 737
-            bvt == 7*units('m'),
-            plamv >= 1.6,
-            taucone == sigskin,
-            3*Qv*(plamv-1) >= Lvmax*bvt*(plamv),
-            lamcone == 0.4, # TODO remove
+            Lvmax                            == 35000*units('N'), # based on 737
+            bvt                              == 7*units('m'),
+            plamv                            >= 1.6,
+            taucone                          == sigskin,
+            3*Qv*(plamv-1)                   >= Lvmax*bvt*(plamv),
+            lamcone                          == 0.4, # TODO remove
             Vcone*(1+lamcone)*(pi+4*thetadb) >= Qv/taucone*(pi+2*thetadb)*(lcone/Rfuse)*2,
-            Wcone >= rhocone*g*Vcone*(1+fstring+fframe)
+            Wcone                            >= rhocone*g*Vcone*(1+fstring+fframe)
 
             # Maximum axial stress model (sum of bending and pressurization strain)
             #rhobend >= rE*(Mh(x)*hfuse)... will be integrated later, since we don't know the forces yet
@@ -267,14 +267,14 @@ class Fuselage(Model):
 #     def __init__(self):
 
 #         # Free variables
-#         W      = Variable('W', 'N', 'Total aircraft weight')
-#         Wfuse  = Variable('W_{fuse}', 'N', 'Fuselage weight')
-#         Wlg    = Variable('W_{lg}', 'N', 'Landing gear weight')
-#         Wvt    = Variable('W_{vt}', 'N', 'Vertical tail weight')
-#         xCG    = Variable('x_{CG}', 'm', 'x-location of CG')
-#         xCGfu  = Variable('x_{CG_{fu}}', 'm', 'x-location of fuselage CG')
-#         xCGlg  = Variable('x_{CG_{lg}}', 'm', 'x-location of landing gear CG')
-#         xCGvt  = Variable('x_{CG_{vt}}', 'm', 'x-location of tail CG') 
+#         W       = Variable('W', 'N', 'Total aircraft weight')
+#         Wfuse   = Variable('W_{fuse}', 'N', 'Fuselage weight')
+#         Wlg     = Variable('W_{lg}', 'N', 'Landing gear weight')
+#         Wvt     = Variable('W_{vt}', 'N', 'Vertical tail weight')
+#         xCG     = Variable('x_{CG}', 'm', 'x-location of CG')
+#         xCGfu   = Variable('x_{CG_{fu}}', 'm', 'x-location of fuselage CG')
+#         xCGlg   = Variable('x_{CG_{lg}}', 'm', 'x-location of landing gear CG')
+#         xCGvt   = Variable('x_{CG_{vt}}', 'm', 'x-location of tail CG') 
 
 #         # Fixed variables (pulled from Philippe's aircraft model)
 #         Weng    = Variable('W_{eng}', 10000, 'N', 'Engine weight')
@@ -304,23 +304,23 @@ if __name__ == "__main__":
     # subs = {'R_{fuse}_Fuselage':4,'w_{fuse}_Fuselage':10}
     #sol = M.localsolve("mosek",tolerance = 0.01, verbosity = 1, iteration_limit=50)
     varVals = sol['variables']
-    print 'Cabin volume: ' + str(varVals['V_{cabin}_Fuselage']) +'.'
-    print 'Fuselage width: ' + str(varVals['w_{fuse}_Fuselage']) + '.'
-    print 'Fuselage length: ' + str(varVals['l_{fuse}'])
-    print 'Floor area: ' + str(varVals['A_{floor}_Fuselage']) + '.'
-    print 'Floor height: ' + str(varVals['h_{floor}_Fuselage']) + '.'
-    print 'Floor length: ' + str(varVals['l_{floor}_Fuselage']) + '.'
-    print  'Fuselage angle: ' + str(varVals['\\theta_{db}_Fuselage']) + '.'
-    print 'Fuselage radius: ' + str(varVals['R_{fuse}_Fuselage']) + '.'
-    print 'Floor total loading: ' + str(varVals['P_{floor}_Fuselage']) + '.'
-    print 'Floor weight: ' + str(varVals['W_{floor}_Fuselage']) + '.'
-    print 'Floor volume: ' + str(varVals['V_{floor}_Fuselage']) + '.'
+    print 'Cabin volume        : ' + str(varVals['V_{cabin}_Fuselage']) +'.'
+    print 'Fuselage width      : ' + str(varVals['w_{fuse}_Fuselage']) + '.'
+    print 'Fuselage length     : ' + str(varVals['l_{fuse}'])
+    print 'Floor area          : ' + str(varVals['A_{floor}_Fuselage']) + '.'
+    print 'Floor height        : ' + str(varVals['h_{floor}_Fuselage']) + '.'
+    print 'Floor length        : ' + str(varVals['l_{floor}_Fuselage']) + '.'
+    print 'Fuselage angle      : ' + str(varVals['\\theta_{db}_Fuselage']) + '.'
+    print 'Fuselage radius     : ' + str(varVals['R_{fuse}_Fuselage']) + '.'
+    print 'Floor total loading : ' + str(varVals['P_{floor}_Fuselage']) + '.'
+    print 'Floor weight        : ' + str(varVals['W_{floor}_Fuselage']) + '.'
+    print 'Floor volume        : ' + str(varVals['V_{floor}_Fuselage']) + '.'
     print 'Floor bending moment: ' + str(varVals['M_{floor}_Fuselage']) + '.'
-    print 'Shell thickness: ' + str(varVals['t_{shell}_Fuselage']) + '.'
-    print 'Skin hoop stress: ' + str(varVals['\\sigma_{\\theta}_Fuselage']) + '.'
-    print 'Skin axial stress: ' + str(varVals['\\sigma_x_Fuselage']) + '.'
-    print 'Cone weight: ' + str(varVals['W_{cone}_Fuselage'])
-    print 'Cone length: ' + str(varVals['l_{cone}_Fuselage'])
-    print 'Cone volume: ' + str(varVals['V_{cone}_Fuselage'])
-    print 'Tail torsion moment: ' + str(varVals['Q_{v}_Fuselage'])
-    print 'Cone taper ratio: ' + str(varVals['\\lambda_{cone}'])
+    print 'Shell thickness     : ' + str(varVals['t_{shell}_Fuselage']) + '.'
+    print 'Skin hoop stress    : ' + str(varVals['\\sigma_{\\theta}_Fuselage']) + '.'
+    print 'Skin axial stress   : ' + str(varVals['\\sigma_x_Fuselage']) + '.'
+    print 'Cone weight         : ' + str(varVals['W_{cone}_Fuselage'])
+    print 'Cone length         : ' + str(varVals['l_{cone}_Fuselage'])
+    print 'Cone volume         : ' + str(varVals['V_{cone}_Fuselage'])
+    print 'Tail torsion moment : ' + str(varVals['Q_{v}_Fuselage'])
+    print 'Cone taper ratio    : ' + str(varVals['\\lambda_{cone}'])
