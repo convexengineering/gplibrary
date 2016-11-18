a5b44420022cfbee53d3093a8d9286c36e7f9ff5
mjburton <mjburton@mit.edu>
2016-11-18 10:06:24 -0500

    make spar loading model more generalized and move to separate file

--- gpkitmodels/aircraft/GP_submodels/capspar.py
+++ gpkitmodels/aircraft/GP_submodels/capspar.py
@@ -1,7 +1,6 @@
 " cap spar "
 from gpkit import Model, Variable, vectorize
-from constant_taper_chord import c_bar
-from beam import Beam
+from chord_spar_loading import ChordSparL
 
 class CapSpar(Model):
     "cap spar model"
@@ -17,44 +16,23 @@ class CapSpar(Model):
             hin = Variable("h_{in}", "in", "inner spar height")
             w = Variable("w", "in", "spar width")
             I = Variable("I", "m^4", "spar x moment of inertia")
+            Sy = Variable("S_y", "m**3", "section modulus")
             dm = Variable("dm", "kg", "segment spar mass")
 
         W = Variable("W", "lbf", "spar weight")
         w_lim = Variable("w_{lim}", 0.15, "-", "spar width to chord ratio")
         g = Variable("g", 9.81, "m/s^2", "gravitational acceleration")
 
-        self.loading = CapSparL
+        self.loading = ChordSparL
 
         constraints = [I <= 2*w*t*(hin/2)**2,
                        dm >= rhocfrp*w*t*b/(self.N-1),
                        W >= 2*dm.sum()*g,
                        w <= w_lim*cave,
                        cave*tau >= hin + 2*t,
+                       Sy*(hin + t) <= I,
                        E == E,
                       ]
 
         Model.__init__(self, None, constraints, **kwargs)
 
-class CapSparL(Model):
-    "spar loading model"
-    def __init__(self, static, Wcent, **kwargs):
-
-        Nmax = Variable("N_{max}", 5, "-", "max loading")
-        cbar = c_bar(0.5, static.N)
-        sigmacfrp = Variable("\\sigma_{CFRP}", 475e6, "Pa", "CFRP max stress")
-        kappa = Variable("\\kappa", 0.2, "-", "max tip deflection ratio")
-        with vectorize(static.N-1):
-            Mr = Variable("M_r", "N*m", "wing section root moment")
-
-        beam = Beam(static.N, cbar)
-
-        constraints = [
-            # dimensionalize moment of inertia and young's modulus
-            beam["\\bar{EI}"] <= (8*static["E"]*static["I"]/Nmax
-                                  / Wcent/static["b"]**2),
-            Mr == (beam["\\bar{M}"][:-1]*Wcent*Nmax*static["b"]/4),
-            sigmacfrp >= Mr*(static["h_{in}"]+static["t"])/static["I"],
-            beam["\\bar{\\delta}"][-1] <= kappa,
-            ]
-
-        Model.__init__(self, None, [beam, constraints], **kwargs)
---
+++ gpkitmodels/aircraft/GP_submodels/chord_spar_loading.py
@@ -0,0 +1,28 @@
+" cap spar "
+from gpkit import Model, Variable, vectorize
+from constant_taper_chord import c_bar
+from beam import Beam
+
+class ChordSparL(Model):
+    "spar loading model"
+    def __init__(self, static, Wcent, **kwargs):
+
+        Nmax = Variable("N_{max}", 5, "-", "max loading")
+        cbar = c_bar(0.5, static.N)
+        sigmacfrp = Variable("\\sigma_{CFRP}", 475e6, "Pa", "CFRP max stress")
+        kappa = Variable("\\kappa", 0.2, "-", "max tip deflection ratio")
+        with vectorize(static.N-1):
+            Mr = Variable("M_r", "N*m", "wing section root moment")
+
+        beam = Beam(static.N, cbar)
+
+        constraints = [
+            # dimensionalize moment of inertia and young's modulus
+            beam["\\bar{EI}"] <= (8*static["E"]*static["I"]/Nmax
+                                  / Wcent/static["b"]**2),
+            Mr == (beam["\\bar{M}"][:-1]*Wcent*Nmax*static["b"]/4),
+            sigmacfrp >= Mr/static["S_y"],
+            beam["\\bar{\\delta}"][-1] <= kappa,
+            ]
+
+        Model.__init__(self, None, [beam, constraints], **kwargs)
