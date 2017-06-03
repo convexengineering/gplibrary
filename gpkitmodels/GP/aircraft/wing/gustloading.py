dde7f3ae362bb71d978d6d6d070b86b01f2f145e
mjburton <mjburton@mit.edu>
2017-06-03 13:25:49 -0400

    add foam and shear web to cap spar model

--- gpkitmodels/GP/aircraft/wing/gustloading.py
+++ gpkitmodels/GP/aircraft/wing/gustloading.py
@@ -17,6 +17,7 @@ class GustL(Model):
         Nmax = Variable("N_{max}", 2, "-", "load safety factor")
         cbar, eta = c_bar(0.5, static.N)
         sigmacfrp = Variable("\\sigma_{CFRP}", 1700e6, "Pa", "CFRP max stress")
+        taucfrp = Variable("\\tau_{CFRP}", 450e6, "Pa", "CFRP fabric stress")
         kappa = Variable("\\kappa", 0.2, "-", "max tip deflection ratio")
 
         with Vectorize(static.N-1):
@@ -46,6 +47,8 @@ class GustL(Model):
             Mr == (beam["\\bar{M}"][:-1]*Wcent*Nmax*static["b"]/4),
             sigmacfrp >= Mr/static["S_y"],
             beam["\\bar{\\delta}"][-1] <= kappa,
+            taucfrp >= (beam["\\bar{S}"][-1]*Wcent*Nmax/4/static["t_{shear}"]
+                        / static["c_{ave}"]/static["\\tau"])
             ]
 
         return beam, constraints
