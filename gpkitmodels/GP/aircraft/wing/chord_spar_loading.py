78ea57f43d6b86cded54e1928b455961cd195d90
mjburton <mjburton@mit.edu>
2017-11-03 14:44:02 -0400

    add new variable W to eliminate need of Wcent in future

--- gpkitmodels/GP/aircraft/wing/chord_spar_loading.py
+++ gpkitmodels/GP/aircraft/wing/chord_spar_loading.py
@@ -29,6 +29,7 @@ class ChordSparL(Model):
         sigmacfrp = Variable("\\sigma_{CFRP}", 1700e6, "Pa", "CFRP max stress")
         taucfrp = Variable("\\tau_{CFRP}", 450e6, "Pa", "CFRP fabric stress")
         kappa = Variable("\\kappa", 0.2, "-", "max tip deflection ratio")
+        W = Variable("W", "lbf", "loading weight")
 
         with Vectorize(static.N-1):
             Mr = Variable("M_r", "N*m", "wing section root moment")
@@ -41,12 +42,13 @@ class ChordSparL(Model):
         constraints = [
             # dimensionalize moment of inertia and young's modulus
             beam["dx"] == static["d\\eta"],
+            W == Wcent,
             beam["\\bar{EI}"] <= (8*static["E"]*static["I"]/Nmax
-                                  / Wcent/static["b"]**2),
-            Mr == (beam["\\bar{M}"][:-1]*Wcent*Nmax*static["b"]/4),
+                                  / W/static["b"]**2),
+            Mr == (beam["\\bar{M}"][:-1]*W*Nmax*static["b"]/4),
             sigmacfrp >= Mr/static["S_y"],
             beam["\\bar{\\delta}"][-1] <= kappa,
-            taucfrp >= (beam["\\bar{S}"][-1]*Wcent*Nmax/4/static["t_{shear}"]
+            taucfrp >= (beam["\\bar{S}"][-1]*W*Nmax/4/static["t_{shear}"]
                         / static["c_{ave}"]/static["\\tau"])
             ]
 
