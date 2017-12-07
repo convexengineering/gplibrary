2a6c0797a25de64d32388474caf828c57a29e96a
bqpd <eburn@mit.edu>
2017-12-07 13:00:02 -0500

    Update qstr.py

--- gpkitmodels/tools/qstr.py
+++ gpkitmodels/tools/qstr.py
@@ -1,5 +1,12 @@
 from gpkit.small_scripts import mag
 
-def qstr(qty):
-    'Converts a pint quantity to a nice string for display'
-    return "%-.4g%s" % (qty.magnitude,  u" {:~}".format(qty.units)) if hasattr(qty, "magnitude") else (qty, "")
+def qstr(qty, fmtstr="%-.4g"):
+    'Converts a number/pint quantity to a nice string for display'
+    if hasattr(qty, "magnitude"):
+        magstr = fmt % qty.magnitude
+        unitstr = u" {:~}".format(qty.units))
+        if "dimensionless" in unitstr:  # boring and long way to put it
+            unitstr = ""
+        return magstr + unitstr
+    else:
+        return fmt % qty  # it's just a number
