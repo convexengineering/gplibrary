4056fbc1686263de757f81250f7598f9e9e02e96
bqpd <eburn@mit.edu>
2017-12-07 12:56:31 -0500

    Update qstr.py

--- gpkitmodels/tools/qstr.py
+++ gpkitmodels/tools/qstr.py
@@ -2,4 +2,4 @@ from gpkit.small_scripts import mag
 
 def qstr(qty):
     'Converts a pint quantity to a nice string for display'
-    return "%-.4g %s" % (qty.magnitude,  u"{:~}".format(qty.units)) if hasattr(qty, "magnitude") else (qty, "")
+    return "%-.4g%s" % (qty.magnitude,  u" {:~}".format(qty.units)) if hasattr(qty, "magnitude") else (qty, "")
