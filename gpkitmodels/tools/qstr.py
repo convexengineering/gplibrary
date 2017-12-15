d0a9444ee98dadf3cf81a7475158ae7ce1a017f5
nillst <scott.t.nill@vanderbilt.edu>
2017-12-15 18:20:57 +0000

    fixed paren mismatch

--- gpkitmodels/tools/qstr.py
+++ gpkitmodels/tools/qstr.py
@@ -4,7 +4,7 @@ def qstr(qty, fmtstr="%-.4g"):
     'Converts a number/pint quantity to a nice string for display'
     if hasattr(qty, "magnitude"):
         magstr = fmt % qty.magnitude
-        unitstr = u" {:~}".format(qty.units))
+        unitstr = u" {:~}".format(qty.units)
         if "dimensionless" in unitstr:  # boring and long way to put it
             unitstr = ""
         return magstr + unitstr
