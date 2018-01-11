957f5ba685463b16c168b2419cec71ffe8dc542a
mjburton <mjburton@mit.edu>
2018-01-11 09:36:53 -0500

    change elliptical fuse model to parse variables

---
+++ gpkitmodels/GP/aircraft/fuselage/test_fuselage.py
@@ -0,0 +1,21 @@
+from gpkit import Model
+from gpkitmodels.GP.aircraft.fuselage.elliptical_fuselage import Fuselage
+from gpkitmodels.GP.aircraft.wing.wing_test import FlightState
+
+def test_ellp():
+    "elliptical fuselage test"
+    f = Fuselage()
+    fs = FlightState()
+    faero = f.flight_model(f, fs)
+    f.substitutions[f.Vol] = 1.33
+
+    m = Model(f.W*faero.Cd, [f, fs, faero])
+    m.solve()
+
+def test():
+    "tests"
+    test_ellp()
+
+if __name__ == "__main__":
+    test()
+
