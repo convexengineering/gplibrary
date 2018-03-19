f30e2eab364eb8b80f4e536e74f6bab88dbbd51b
Chris Courtin <courtin@MIT.EDU>
2018-03-18 21:25:33 -0400

    Add prop structure and performance models

--- gpkitmodels/GP/aircraft/prop/propeller.py
+++ gpkitmodels/GP/aircraft/prop/propeller.py
@@ -1,15 +1,28 @@
 " propeller model "
 from numpy import pi
-from gpkit import Model, parse_variables
+from gpkit import Model, parse_variables, SignomialsEnabled, SignomialEquality
 
 class Propeller(Model):
     """ Propeller Model
 
     Variables
     ---------
+    R          10           [m]         prop radius
+
+    """
+    def setup(self):
+        exec parse_variables(Propeller.__doc__)
+
+    def performance(state):
+        return Propeller_Performance(self, state)
+
+class Propeller_Performance(Model):
+    """ Propeller Model
+
+    Variables
+    ---------
     T                       [N]         thrust
     Tc                      [-]         coefficient of thrust
-    R          0.3          [m]         prop radius
     etaadd     0.7          [-]         swirl and nonuniformity losses
     etav       0.85         [-]         viscous losses
     etai                    [-]         inviscid losses
@@ -27,8 +40,10 @@ class Propeller(Model):
 
         V = state.V
         rho = state.rho
+        
 
-        return [eta <= etav*etai,
+        constraints = [eta <= etav*etai,
                 Tc == T/(0.5*rho*V**2*pi*R**2),
-                z2 >= 1 + Tc,
+                z2 >= Tc + 1,
                 etai*(z1 + z2**0.5/etaadd) <= 2]
+        return constraints
