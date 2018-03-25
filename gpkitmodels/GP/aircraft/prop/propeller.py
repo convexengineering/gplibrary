eb7c39d6c7341c9b2c1bf70f8d2485c53192305b
Chris Courtin <courtin@MIT.EDU>
2018-03-25 09:49:53 -0400

    Updated propeller to base and performance class

--- gpkitmodels/GP/aircraft/prop/propeller.py
+++ gpkitmodels/GP/aircraft/prop/propeller.py
@@ -14,7 +14,7 @@ class Propeller(Model):
     def setup(self):
         exec parse_variables(Propeller.__doc__)
 
-    def performance(state):
+    def performance(self,state):
         return Propeller_Performance(self, state)
 
 class Propeller_Performance(Model):
@@ -30,6 +30,11 @@ class Propeller_Performance(Model):
     eta                     [-]         overall efficiency
     z1         self.helper  [-]         efficiency helper 1
     z2                      [-]         efficiency helper 2
+    lam                     [-]         advance ratio
+    CT                      [-]         thrust coefficient
+    CP                      [-]         power coefficient
+    Q                       [N*m]       torque
+    omega                   [RPM]       propeller rotation rate 
 
     """
 
@@ -37,7 +42,7 @@ class Propeller_Performance(Model):
         return 2. - 1./c[self.etaadd]
 
     def setup(self,parent,  state):
-        exec parse_variables(Propeller.__doc__)
+        exec parse_variables(Propeller_Performance.__doc__)
 
         V       = state.V
         rho     = state.rho
@@ -46,5 +51,10 @@ class Propeller_Performance(Model):
         constraints = [eta <= etav*etai,
                 Tc == T/(0.5*rho*V**2*pi*R**2),
                 z2 >= Tc + 1,
-                etai*(z1 + z2**0.5/etaadd) <= 2]
+                etai*(z1 + z2**0.5/etaadd) <= 2,
+                lam == V/(omega*R),
+                CT == Tc*lam**2,
+                CP == Q*omega/(.5*rho*(omega*R)**3*math.pi*R**2),
+                eta == CT*lam/CP, #Is this the same eta? check 
+                ]
         return constraints
