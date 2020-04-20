3c304d8d9fcac32d957801227f2acb0dae5d273c
1ozturkbe <1ozturkbe@gmail.com>
2020-04-20 16:58:51 -0400

    remove main stuff.

--- gpkitmodels/SP/SimPleAC/SimPleAC_mission.py
+++ gpkitmodels/SP/SimPleAC/SimPleAC_mission.py
@@ -326,20 +326,7 @@ def test():
         'T/O factor_m'   :2,
     })
     m.cost = m['W_{f_m}']*units('1/N') + m['C_m']*m['t_m']
-    sol = m.localsolve(verbosity = 2)
-
+    sol = m.localsolve(verbosity = 0)
 
 if __name__ == "__main__":
-    # Most basic way to execute the model 
-    m = Mission(SimPleAC(),4)
-    m.substitutions.update({
-        'h_{cruise_m}'   :5000*units('m'),
-        'Range_m'        :3000*units('km'),
-        'W_{p_m}'        :3000*units('N'),
-        '\\rho_{p_m}'    :1500*units('kg/m^3'),
-        'C_m'            :120*units('1/hr'),
-        'V_{min_m}'      :35*units('m/s'),
-        'T/O factor_m'   :2,
-    })
-    m.cost = m['W_{f_m}']*units('1/N') + m['C_m']*m['t_m']
-    sol = m.localsolve(verbosity = 0)
+    test()
